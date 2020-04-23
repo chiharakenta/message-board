2fb2e8759513b5a966a35d9a386231f817aea9fe
chiharakenta <chiharakenta0519@gmail.com>
2020-04-23 18:12:19 +0900

    fix database config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
-require('dotenv').config(); // this is important!
+require('dotenv').config();
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": "root",
+    "password": null,
+    "database": "message_board_development",
+    "host": "127.0.0.1",
     "dialect": "mysql",
     "operatorsAliases": "0"
   },
