eef48b09b7c2547fdf0c920258fe4dea817a6d30
chiharakenta <chiharakenta0519@gmail.com>
2020-05-13 16:37:12 +0900

    add user like index

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const db = require('../models/index');
 
 router.get('/new', usersController.new);
 router.post('/', usersController.create);
+
+router.get('/:id/likes', (req, res) => {
+  const options = {
+    include: [
+      {
+        model: db.message,
+        through: db.user_message
+      }
+    ]
+  };
+  db.user.findByPk(req.params.id, options).then((results) => {
+    res.render('users/likes.ejs', {user: results, loggedIn: !!req.user});
+  });
+});
 
 module.exports = router;
