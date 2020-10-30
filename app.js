ea10f4cc64d64504b4e5234ddc051a5d5a7237bd
chiharakenta <chiharakenta0519@gmail.com>
2020-10-31 07:50:37 +0900

    add singin and signup

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var createError = require('http-errors');
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var app = express();
 
 // view engine setup
@@ -14,6 +17,56 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const db = require('./models/index');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+  db.user.findByPk(id)
+  .then(function(user) {
+    done(null, user)
+  })
+  .catch(function(error) {
+    done(error, null)
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(name, password, done) {
+    db.user.findOne({
+      where: {
+        name: name
+      }
+    })
+    .then(function(user) {
+      console.log(user);
+
+      if (!user) {
+        return done(null, false, { message: '入力された名前のユーザーは存在しません。' });
+      }
+      if (user.password != password) {
+        return done(null, false, { message: 'パスワードが一致しません。' });
+      }
+      return done(null, user);
+    })
+    .catch(function(err) {
+      if (err) { return done(err); }
+    });
+  }
+));
 
 // settings for method-override
 var methodOverride = require('method-override')
@@ -28,7 +81,7 @@ app.use(methodOverride(function (req, res) {
 
 // routing
 app.get('/', (req, res) => {
-  res.redirect('/messages');
+  return res.redirect('/messages');
 });
 
 const messagesRouter = require('./routes/messages');
@@ -36,6 +89,33 @@ const repliesRouter = require('./routes/replies');
 app.use('/messages', messagesRouter);
 app.use('/replies', repliesRouter);
 
+app.get('/signup', function(req, res) {
+  res.render('users/signup');
+});
+
+app.post('/signup', function(req, res) {
+  const values = {
+    name: req.body.name,
+    password: req.body.password
+  };
+  db.user.create(values).then(function(results) {
+    res.redirect('/messages');
+  });
+});
+
+app.get('/signin', function(req, res) {
+  res.render('users/signin');
+});
+
+app.post('/signin', passport.authenticate('local', {
+  successRedirect: '/messages',
+  failureRedirect: '/signin'
+}));
+
+app.delete('/signout', function(req, res) {
+  req.logout();
+  res.redirect('/signin');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
